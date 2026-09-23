26ad1236f6617e8f19b582a0d015f83e86ed6ae3
Eshan <eshan.guha@gmail.com>
2026-09-23 18:11:24 -0400

    new file for motmot functions

---
+++ Core/Src/motmot.c
@@ -0,0 +1,10 @@
+// State Estimation/Attitude function (take the sensors and convert into pitch, yaw, roll (i think thats what we need but not sure))
+
+
+//PID Control (control algorithm to make the rocket go straight)
+
+
+//Canard Allocation (take torques and make actual servo angles)
+
+
+//Data Logging and Actuation (write the data we need and actually write the servo)
